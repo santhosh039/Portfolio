d8e029f9214eaf022a64ab65aea0775d8f3e7401
santhosh039 <repalasanthoshkumar@gmail.com>
2021-09-09 20:57:28 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,26 @@
+$(document).ready(function(){
+    $(window).scroll(function(){
+        if(this.scrollY > 20){
+            $('.navbar').addClass("sticky");
+        }
+        else{
+            $('.navbar').removeClass("sticky");
+        }
+                // scroll-up button show/hide script
+                if(this.scrollY > 500){
+                    $('.scroll-up-btn').addClass("show");
+                }else{
+                    $('.scroll-up-btn').removeClass("show");
+                }
+    });
+    $('.navbar .menu li a').click(function(){
+        // applying again smooth scroll on menu items click
+        $('html').css("scrollBehavior", "smooth");
+    });
+    //toggle menu/navbar script
+    $('.menu-btn').click(function(){
+        $('.navbar .menu').toggleClass("active");
+        $('.menu-btn i').toggleClass("active");
+    });
+
+});
